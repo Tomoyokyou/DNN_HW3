2e16d74b86fd9a331e7438a5bddab75786f689b6
Hui <hui811116@outlook.com>
2015-05-12 12:44:58 +0800

    server path set..

--- predict.sh
+++ predict.sh
@@ -1,8 +1,8 @@
-DATADIR=/home/hui/project/model/
+DATADIR=/home/hui/model/
 TYPE=fbank/
-TESTFILE=test/test_norm.svm
-TESTARK=${DATADIR}${TYPE}test.ark
-MAP=${DATADIR}phones/char_48to39.map
+TESTFILE=${DATADIR}test/test_norm.svm
+TESTARK=${DATADIR}test/test.ark
+MAP=${DATADIR}char_48to39.map
 OUTCSV=out.csv
 MODEL=model/new_single_e50.mdl
 
