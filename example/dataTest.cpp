d3373dd5340f81b57e71910cc7567e21bfbaf9c0
Hui <hui811116@outlook.com>
2015-05-26 21:41:52 +0800

    validate inside

--- example/dataTest.cpp
+++ example/dataTest.cpp
@@ -11,10 +11,10 @@ int main(){
 	//string featurePath = "/home/larry/Documents/MLDS/DNN_HW3/model/word_vector.txt";
 	//string sntPath = "/home/jason/training_pre4.txt";
 	//string classPath = "/home/larry/Documents/MLDS/DNN_HW3/model/classes.txt";
-	string featurePath = "/home/hui/project/rnnFeat/word_vector.txt";
-	string sntPath = "/home/hui/project/rnnFeat/training_oov.txt";
-	string classPath = "/home/hui/project/rnnFeat/classes.sorted.txt";
-	string testPath = "/home/hui/project/rnnFeat/testing_data_parse2.txt";
+	string featurePath = "/home/ahpan/DNN_HW3/model/preprocess_3/word_vector.txt";
+	string sntPath = "/home/ahpan/DNN_HW3/model/preprocess_3/training_oov.txt";
+	string classPath = "/home/ahpan/DNN_HW3/model/preprocess_3/classes.sorted.txt";
+	string testPath = "/home/ahpam/DNN_HW3/model/preprocess_3/testing_data_parse2.txt";
 	Dataset d(featurePath.c_str(), classPath.c_str(), sntPath.c_str());
 	d.parseTestData(testPath.c_str());
 	cout << d.getTestSentNum() << endl;
