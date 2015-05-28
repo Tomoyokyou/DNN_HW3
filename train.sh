ed3f69046231c1fb6b2371b0e084b48d648c84c6
Huang Teng-Hui <hui811116@outlook.com>
2015-05-28 21:37:12 +0800

    hui local

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
-FEATUREDIR=/home/ahpan/DNN_HW3/model/preprocess_3/
+#FEATUREDIR=/home/ahpan/DNN_HW3/model/preprocess_3/
+FEATUREDIR=/home/hui/project/rnnFeat/
 FEATUREFILE=${FEATUREDIR}word_vector.txt
 SENTENCEFILE=${FEATUREDIR}training_oov.txt
 CLASSFILE=${FEATUREDIR}classes.sorted.txt
 TESTFILE=${FEATUREDIR}testing_data_parse2.txt
 #ANSDIR=/home/ahpan/Data/
-#FEATUREDIR=/home/hui/project/rnnFeat/
 #ANSDIR=/home/hui/project/rnnFeat/
 #FEATUREDIR=model/
 #ANSDIR=model/
@@ -14,18 +14,19 @@ TESTFILE=${FEATUREDIR}testing_data_parse2.txt
 #TESTFILE=${FEATUREDIR}testing_data_parse2.txt
 #ANSWERFILE=${ANSDIR}answer.txt
 RATE=0.01
-MOMENTUM=0
-EPOCH=1
+MOMENTUM=0.8
+EPOCH=100
 DECAY=0.99
 VAR=0.2
-STEP=5
+STEP=4
 HIDDEN=50
-HIDNUM=1
+HIDNUM=2
 REG=0
-OUT=./model/out.mdl
+OUT=./model/dRNN4.mdl
 CUTCLASS=50
 
 mkdir -p model
+mkdir -p log
 
 if [ -f ./bin/train.app ]; then
 echo "executables checked..."
@@ -35,4 +36,4 @@ make train
 fi
 ./bin/train.app  ${FEATUREFILE} ${SENTENCEFILE} ${CLASSFILE} ${TESTFILE} --ans ${ANSWERFILE} --rate ${RATE} \
 --momentum ${MOMENTUM} --epoch ${EPOCH} --decay ${DECAY} --var ${VAR} --step ${STEP} --reg ${REG} \
- --hidden ${HIDDEN} --outF ${OUT} --cutClass ${CUTCLASS} --hidnum ${HIDNUM}
+ --hidden ${HIDDEN} --outF ${OUT} --cutClass ${CUTCLASS} --hidnum ${HIDNUM} | tee log/dRnn${STEP}.log
